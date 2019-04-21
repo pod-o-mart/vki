4a3fdda307bb2bb056e8494a17870f8ffece1fc1
Martin <webmaster@podolak.net>
2019-04-21 11:14:36 +0000

    Add files via upload

---
+++ keyboard-touch.js
@@ -0,0 +1,9 @@
+/* In order to make the script work, add the declaration
+ * data-disable-touch-keyboard=""
+ * to your input field or textarea */
+$(document).ready(function() {
+	if (Modernizr.touch) {
+		$('[data-disable-touch-keyboard]').attr('readonly', 'readonly');
+		$("head").append($("<link rel='stylesheet' href='keyboard-touch.css' type='text/css' media='screen' />"));
+	}
+});
